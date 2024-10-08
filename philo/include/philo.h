32d44ffa19cd739758d377fbe4f8e2584f47978c
Helena Utzig <helenautzig@gmail.com>
2024-10-08 14:31:55 +0300

    header with libraries for external functions

---
+++ philo/include/philo.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 14:03:13 by hutzig            #+#    #+#             */
+/*   Updated: 2024/10/08 14:30:44 by hutzig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/**
+ * string.h 	: memset,
+ * stdio.h	: printf,
+ * unistd.h	: write, usleep
+ * stdlib.h	: malloc, free
+ * pthread.h	: thread functions 
+ * sys/time.h	: gettimeofday, timeval
+ */
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+
+#endif
