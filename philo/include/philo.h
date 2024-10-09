0355d8a0d907b6e477e4f5fe3507bf76eb2c04d2
Helena Utzig <helenautzig@gmail.com>
2024-10-09 15:02:07 +0300

    arguments validity and data args initialized

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:03:13 by hutzig            #+#    #+#             */
-/*   Updated: 2024/10/08 16:38:42 by hutzig           ###   ########.fr       */
+/*   Updated: 2024/10/09 11:25:44 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,20 @@
 # include <stdlib.h>
 # include <pthread.h>
 # include <sys/time.h>
+
+typedef struct	s_args
+{
+	int	n_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	n_meals;
+}		t_args;
+
+typedef struct	s_data
+{
+	t_args	arg;
+}		t_data;
 
 int	ft_atoi(char *str);
 size_t	ft_strlen(char *str);
--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,56 +6,73 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 16:16:46 by hutzig            #+#    #+#             */
-/*   Updated: 2024/10/08 17:35:50 by hutzig           ###   ########.fr       */
+/*   Updated: 2024/10/09 13:21:12 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_data_args(data, int i, int value)
+void	init_data_args(t_data *data, int i, int nb)
 {
-
+	if (i == 1)
+		data->arg.n_philo = nb;
+	else if (i == 2)
+		data->arg.time_to_die = nb;
+	else if (i == 3)
+		data->arg.time_to_eat = nb;
+	else if (i == 4)
+		data->arg.time_to_sleep = nb;
+	else if (i == 5)
+		data->arg.n_meals = nb;
+	else
+		data->arg.n_meals = 0;
 }
 
-int	get_arguments(t_philo *data, int argc, char **argv)
+int	get_arguments(t_data *data, int argc, char **argv)
 {
 	int	i;
 	int	tmp;
 
 	i = 0;
-	while  (++i > argc)
+	while (++i < argc)
 	{
 		tmp = ft_atoi(argv[i]);
 		if (tmp <= 0)
 		{
 			ft_putstr_fd(argv[i], 2);
-			ft_putstr_fd(" : Invalid argument\n", 2);
+			ft_putstr_fd(": Invalid argument\n", 2);
 			return (EXIT_FAILURE);
 		}
-		init_data_args(); // working on this function to 
-		if (i == 1)
-			data->n_philo = tmp;
-		else if (i == 2)
-			data->time_to_die = tmp;
-		else if (i == 3)
-			data->time_to_eat = tmp;
-		else if (i == 4)
-			data->time_to_sleep = tmp;
-		else if (i == 5)
-			data->must_eat = tmp;
-		else
-			data->must_eat = 0;
+		init_data_args(data, i, tmp);
 	}
+	return (EXIT_SUCCESS);
 }
 
 int	main(int argc, char **argv)
 {
+	t_data	*data;
+	
 	if (argc != 5 && argc != 6)
 	{
 		ft_putstr_fd("Invalid number of argument\n", 2);
 		return (EXIT_FAILURE);
 	}
-	if (get_arguments(data, argc, argv));
+	data = (t_data *)malloc(sizeof(t_data));
+	if (!data)
+	{
+    		ft_putstr_fd("Memory allocation failed\n", 2);
+    		return (EXIT_FAILURE);
+	}
+	if (get_arguments(data, argc, argv))
+	{
+		free(data);
 		return (EXIT_FAILURE);
-	return (0);
+	}
+	//printf("%d\tn_philo\n", data->arg.n_philo);
+	//printf("%d\ttime_to_die\n", data->arg.time_to_die);
+	//printf("%d\ttime_to_eat\n", data->arg.time_to_eat);
+	//printf("%d\ttime_to_sleep\n", data->arg.time_to_sleep);
+	//printf("%d\tn_meals\n", data->arg.n_meals);
+	free(data);
+	return (EXIT_SUCCESS);
 }
