36a25d0ba98ebcb020f8e165efa9af3012417ecc
Helena Utzig <helenautzig@gmail.com>
2024-10-22 08:48:19 +0300

    init threads

--- philo/sources/threads.c
+++ philo/sources/threads.c
@@ -6,8 +6,33 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 10:23:18 by hutzig            #+#    #+#             */
-/*   Updated: 2024/10/17 10:23:29 by hutzig           ###   ########.fr       */
+/*   Updated: 2024/10/22 08:47:40 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	set_up_threads(t_data *data)
+{
+	pthread_t	*th;
+	int		i;
+
+	th = (pthread_t *)malloc(sizeof(pthread) * data->n_philo);
+	if (!th)
+	{
+    		ft_putstr_fd("Memory allocation failed\n", 2);
+    		return (EXIT_FAILURE);
+	}
+	i = 0;
+	while (i < data->n_philo)
+	{
+		if (pthread_create(&th[i], NULL, *new_routine, &data->philo[i]) != 0)
+		{
+    			ft_putstr_fd("Pthread failed to be created\n", 2);
+			free(th);
+			return (EXIT_FAILURE);
+		}
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
